118ba31491e656fc49fee5b911d59d82b28c25e5
bjmashele <bjmashele@gmail.com>
2018-06-08 13:33:21 -0500

    Added app's basic routes

--- src/main.js
+++ src/main.js
@@ -1,7 +1,47 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+//env config
+if (process.env.NODE_ENV === 'production') {
+  window.endpoint = "https:/afrolily.heroku.com";
+} else {
+  window.endpoint = "http://localhost:3000";
+}
+
+//Global vars
+window.moment = require('moment');
+window.axios = require('axios');
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+//Local dependensies
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import App from './components/App.vue'
+import Product from './components/Product.vue'
+import Order from './components/Order.vue'
+import Complete from './components/Complete.vue'
+
+// Routes
+const routes = [
+  { name: 'home', path: '/', component: Product },
+  { name: 'order', path: '/order', component: Order },
+  { name: 'order-complete', path: '/order-complete/:id', component: Complete }
+];
+
+//Register Routes
+const router = new VueRouter({
+  routes
+});
+
+// Instantiate application to the DOM
 new Vue({
+  router,
   el: '#app',
+  data(){
+      return {
+          order_details: {}
+      }
+  },
   render: h => h(App)
-})
+});
